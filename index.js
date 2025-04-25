b64c3d692b12168ad3269ca956a35a2522974c70
crindshelter <crindshelter@gmail.com>
2025-04-25 19:25:31 +0800

    Prevents consecutive operators/decimal in display.

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const textScreen = document.querySelector(".text");
+let currentInput = "";
 
 function main(){
 
 }
 
+
+
 // Display numbers/operator on screen
 function display(number) {
-    textScreen.innerHTML += number;
+
+    // Prevents multiple operators
+    if (isNaN(number) && !isDecimal(number)) {
+        if (currentInput !== "" && !isOperator(currentInput.charAt(currentInput.length - 1))) {
+            currentInput += number;  
+        }
+    } else if (isDecimal(number)) {
+        if (!currentInput.includes(".")) {
+            currentInput += number;
+        }
+    } else {
+        currentInput += number;
+    }
+    textScreen.innerHTML = currentInput;
 }
 
+// Clears display
 function clearDisplay() {
+    currentInput = "";
     textScreen.innerHTML = "";
 }
 
+// Deletes text
 function deleteText() {
-    textScreen.innerHTML = textScreen.innerHTML.slice(0, -1);
+    currentInput = currentInput.slice(0, -1);  
+    textScreen.innerHTML = currentInput;
 }
 
 // Adds two numbers
@@ -37,5 +57,12 @@ function divide(x, y) {
     return x / y;
 }
 
+function isOperator(char) {
+    return ["+", "-", "*", "/"].includes(char);
+}
+
+function isDecimal(char) {
+    return char === ".";
+}
 
 main()
