a02cec9531ca4b98b49e87b1c7e3188e0872478b
crindshelter <crindshelter@gmail.com>
2025-04-25 17:52:29 +0800

    Added all buttons

---
+++ index.js
@@ -0,0 +1,25 @@
+function main(){
+
+}
+
+// Adds two numbers
+function add(x, y) {
+    return x + y;
+}
+
+// Subtract two numbers
+function subtract(x, y) {
+    return x - y; 
+}
+
+// Multiplies two numbers
+function multiply(x, y) {
+    return x * y;
+}
+
+// Divides two numbers
+function divide(x, y) {
+    return x / y;
+}
+
+main()
